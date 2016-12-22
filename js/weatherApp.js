3cb83787aa27b27d2103b6374abe985bdb0c2909
tolgaparlan <tolgaparlan@gmail.com>
2016-12-22 03:31:55 +0100

    changed the API because openweather wasn't using https

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -7,8 +7,8 @@ var weatherDisplay = function(){
     var coordinates = "";
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(function(position){
-            coordinates += "lat="+position.coords.latitude;
-            coordinates += "&lon="+position.coords.longitude;
+            coordinates += position.coords.latitude+',';
+            coordinates += position.coords.longitude;
             dataGrab(coordinates);
         });
     }        
@@ -16,42 +16,43 @@ var weatherDisplay = function(){
 
 //get the weather data from the API
 var dataGrab = function(loc){
-    var url = "http://api.openweathermap.org/data/2.5/weather?" + loc + "&appid=76d306923f93e462d87c1a1d0c9b6ec6&units=metric";
-    $("#weatherID").text(url);
+    var time = Math.floor(Date.now()/1000); //not a typo. this returns a unix timestamp
+    console.log(time);
+    var unit = "si"; //add option to switch to fahrenheit
+    var url = "https://api.darksky.net/forecast/9b7da12d061643778caa8aff7c3b85cf/" + loc + ","+time+"?callback=?&units="+unit+"&exclude=flags,hourly";
     $.getJSON(url,function(data){
-        dataPlacement(data);
+        dataPlacement(data,loc);
     });
 }
 
 //place the weather data into HTML elements
-var dataPlacement = function(data){
-    var keys = Object.keys(data);
+var dataPlacement = function(data,loc){
     console.log(data);
-    
-    //get all major weather events from the weather array and add them to HTML
-    var dataHold = "";//one variable to hold them all, and bind them in the HTMl
-    for(var i=0;i<data["weather"].length;i++){
-        dataHold += data["weather"][i]["main"] + " / "; 
-    }
-    $("#weather").text(dataHold);
+    //main weather event
+    $("#weather").text(data["currently"]["summary"]);
     
     //do the same for the descriptions
-    dataHold = "";
-    for(var i=0;i<data["weather"].length;i++){
-        dataHold += data["weather"][i]["description"] + " / "; 
-    }
-    $("#weather-description").text(dataHold);
+    $("#weather-description").text(data["daily"]["data"][0]["summary"]);
     
     //temperatures
-    $("#temperature-current").text("Temp: " + data["main"]["temp"]);
-    $("#temperature-maxmin").text("Temp max/min: " + data["main"]["temp_max"] + "/" + data["main"]["temp_min"]);
+    $("#temperature-current").text("Temp: " + Math.floor(data["currently"]["temperature"]));
+    $("#temperature-maxmin").text("Temp max/min: " + Math.floor(data["daily"]["data"][0]["temperatureMax"]) + "/" + Math.floor(data["daily"]["data"][0]["temperatureMin"]));    
     
+    //the address 
+    getAddress(loc);
     
-    //place the address data
-    $("#address").text(data["name"]+ "/"+data["sys"]["country"]);
     //wind speed
-    $("#wind").text("Wind: "+data["wind"]["speed"]);
+    $("#wind").text("Wind Speed: "+data["currently"]["windSpeed"]);
 }
 
-
+//get the address from the google maps API
+var getAddress = function(loc){
+    $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng="+loc+"&result_type=political&key=AIzaSyBsD60C9sqevu_5EXhyl_bCphqoGLBO8do",function(data){
+        setAddress(data);
+    });
+}
+var setAddress = function(address){    
+    //place the address data
+    $("#address").text(address["results"][0]["formatted_address"]);
+}
 
