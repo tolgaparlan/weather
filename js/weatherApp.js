cc7fa8385ff76cb21575ad8a0beb62829750f7fb
tolgaparlan <tolgaparlan@gmail.com>
2016-12-22 14:24:25 +0100

    added google location api back as it's way more accurate

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -4,38 +4,60 @@ $(document).ready(function() {
 
 //get the user location and put in a suitable format for the API
 var weatherDisplay = function(){
-    $.getJSON('https://ipinfo.io', function(data){
-        dataGrab(data["loc"],data["city"]+"/"+data["region"]+"/"+data["country"]); //acquire the weather data for the given location
-    })         
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(loc){
+            dataGrab(loc["coords"]["latitude"]+","+loc["coords"]["longitude"]); //acquire the weather data for the given location
+        });
+    }
 }; 
 
 //get the weather data from the API
-var dataGrab = function(loc,address){
+var dataGrab = function(loc){
     var time = Math.floor(Date.now()/1000); //returns a unix timestamp
-    console.log(time);
-    var unit = "si"; //add option to switch to fahrenheit
+    var unit = "ca"; //add option to switch to fahrenheit
     var url = "https://api.darksky.net/forecast/9b7da12d061643778caa8aff7c3b85cf/" + loc + ","+time+"?callback=?&units="+unit+"&exclude=flags,hourly";
     $.getJSON(url,function(data){
-        dataPlacement(data,loc,address);
+        dataPlacement(data,loc);
+        console.log(data);
     });
 }
 
 //place the weather data into HTML elements
-var dataPlacement = function(data,loc,address){
-    console.log(data);
+var dataPlacement = function(data,loc){
     //main weather event
     $("#weather").text(data["currently"]["summary"]);
     
     //do the same for the descriptions
     $("#weather-description").text(data["daily"]["data"][0]["summary"]);
     
+    //daily rain chance
+    $("#rain-chance").text("Rain Chance: "+data["daily"]["data"][0]["precipProbability"]*100+"%");
+    
     //temperatures
     $("#temperature-current").text("Temp: " + Math.floor(data["currently"]["temperature"]));
     $("#temperature-maxmin").text("Temp max/min: " + Math.floor(data["daily"]["data"][0]["temperatureMax"]) + "/" + Math.floor(data["daily"]["data"][0]["temperatureMin"]));    
     
     //the address 
-    $("#address").text(address);
+    getAddress(loc);
     
     //wind speed
-    $("#wind").text("Wind Speed: "+data["currently"]["windSpeed"]);
+    $("#wind").text("Wind Speed: "+data["daily"]["data"][0]["windSpeed"]);
+    
+    //icon    
+    var skycons = new Skycons({"color": "black"});
+
+    // you can add a canvas by it's ID...
+    skycons.add("weather-icon", data["currently"]["icon"]);
+    
+}
+
+//get the address from the google maps API
+var getAddress = function(loc){
+    $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng="+loc+"&result_type=political&key=AIzaSyBsD60C9sqevu_5EXhyl_bCphqoGLBO8do",function(data){
+        setAddress(data);
+    });
+}
+var setAddress = function(address){    
+    //place the address data
+    $("#address").text(address["results"][0]["formatted_address"]);
 }
